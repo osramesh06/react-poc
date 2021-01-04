906e1e5a62a3a33e34c579160af0011d384fda94
Ramesh Kumar <osrameshkumar06@gmail.com>
2021-01-04 12:46:55 +0530

    changes in files

--- src/ExcelUploadWithValidation/ExcelUploadWithValidation.js
+++ src/ExcelUploadWithValidation/ExcelUploadWithValidation.js
@@ -1,11 +1,13 @@
 import React, { useRef, useState } from "react";
-import { Modal } from "antd";
+import { Modal, notification } from "antd";
 import readXlsxFile from "read-excel-file";
 
 const schema = {
   name: {
     prop: "name",
     type: String,
+    required: true,
+    title: "Name",
     // Excel stores dates as integers.
     // E.g. '24/03/2018' === 43183.
     // Such dates are parsed to UTC+0 timezone with time 12:00 .
@@ -31,31 +33,57 @@ const ExcelUploadWithValidation = () => {
   const [excelErr, setExcelErr] = useState([]);
   const inputRef = useRef();
   const handleChange = (e) => {
-    readXlsxFile(e.target.files[0], { schema }).then((rows, err) => {
-      console.log(rows, err);
-      if (rows.errors.length > 0) {
-        setVisible(true);
-        setExcelErr(rows.errors);
-      }
+    new Promise((resolve) => {
+      readXlsxFile(e.target.files[0]).then((res) => {
+        if (res.length > 0) {
+          let headingsColumn = res[0];
+          let missedColumn = [];
+          for (let [key, value] of Object.entries(schema)) {
+            if (headingsColumn.includes(key) === false) {
+              missedColumn.push(key);
+            }
+          }
+          if (missedColumn.length > 0) {
+            notification.error({
+              message: "Column are Missed",
+              description: `${missedColumn.toString()} this column are missed in this excel file upload correct excel file`,
+            });
+          } else {
+            resolve();
+          }
+        }
+      });
+    }).then(() => {
+      readXlsxFile(e.target.files[0], { schema }).then((rows, err) => {
+        if (rows.errors.length > 0) {
+          setVisible(true);
+          setExcelErr(rows.errors);
+        }
+      });
     });
   };
 
   const handleClose = () => {
     setVisible(false);
-    inputRef.current.value = ''
+    inputRef.current.value = "";
   };
 
   return (
     <>
-      <input type="file" ref={inputRef} onChange={handleChange} />
+      <input
+        type="file"
+        ref={inputRef}
+        onChange={handleChange}
+        accept=".xlsx, .xls"
+      />
       <Modal
-        title="Basic Modal"
+        title="Mismatched values rows"
         visible={isVisble}
         onOk={handleClose}
         onCancel={handleClose}
       >
-        {excelErr.map((list) => (
-          <p>
+        {excelErr.map((list, i) => (
+          <p key={i}>
             In row {list.row} {list.column} column data is invalid value{" "}
             {list.value}
           </p>
